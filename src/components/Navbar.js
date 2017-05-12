d9452af333a6925046751481a80de6ccf7a8c0bc
JoshMarsden94 <joshmarsden94@outlook.com>
2017-05-12 12:54:51 +0100

    CSS improvements to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ import { Component } from 'react';
 class Navbar extends Component {
   render () {
     return (
-        <section className="hero is-primary">
+        <section className="hero is-primary" id="heading-container">
         <div className="hero-body">
             <div className="container">
                 <Link to="/" id='nav'><img className="northcoders-logo" src="https://northcoders.com/images/logos/learn_to_code_manchester_rw_original.png" alt="Northcoders logo"></img></Link>
-            <h2 className="nav-heading title is-3">
+            <h2 className="nav-heading title is-3" id="nav-head">
                 Northcoders News
             </h2>
             </div>
